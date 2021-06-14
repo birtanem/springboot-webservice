a6555915a67227c161dae6db9d771e5290807325
birtanem <eneppy@gmail.com>
2021-06-14 23:55:53 +0900

    게시글 수정 화면 만들기

--- src/main/java/com/springboot/book/domain/posts/PostsRepository.java
+++ src/main/java/com/springboot/book/domain/posts/PostsRepository.java
@@ -1,10 +1,16 @@
 package com.springboot.book.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 // --- DAO라고 불리는 DB Layer접근자
 // jpa에서는 Repository라고 부르며 인터페이스로 생성함.
 // 인터페이스 생성 후 JpaRepository<Entity클래스, PK타입>을 상속하면 자동으로 CRUD가 생성된다.
 // @Repository를 추가할 필요가 없다. Entity클래스와 기본 Entity Repository는 같은 위치에 둔다.
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/springboot/book/service/posts/PostsService.java
+++ src/main/java/com/springboot/book/service/posts/PostsService.java
@@ -3,13 +3,16 @@ package com.springboot.book.service.posts;
 
 import com.springboot.book.domain.posts.Posts;
 import com.springboot.book.domain.posts.PostsRepository;
+import com.springboot.book.web.dto.PostsListResponseDto;
 import com.springboot.book.web.dto.PostsResponseDto;
 import com.springboot.book.web.dto.PostsSaveRequestDto;
 import com.springboot.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값을 하는 생성자를 생성해준다.
 @Service
@@ -34,6 +37,15 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
         return new PostsResponseDto(entity);
+    }
+
+    //readOnly 옵션추가. 트랜잭션 범위는 유지하고 조회기능만 남겨두어 조회속도가 개선됨. 등록,수정,삭제 기능이 없는 서비스메서드에서 사용할 것.
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+        //postsRepository의 결과로 넘어온 Posts의 Stream을 map을 통해 PostsListResponseDto변환 -> List반환
     }
     //update기능에서 db에 쿼리를 날리는 부분이 없는 이유: JPA의 영속성 컨텍스트(엔티티를 영구 저장하는 환경)
 }
