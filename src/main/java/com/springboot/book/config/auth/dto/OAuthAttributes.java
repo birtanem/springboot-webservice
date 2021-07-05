a3d181998d5f4cac83e9056538974c0b16b4919e
birtanem <eneppy@gmail.com>
2021-07-06 00:36:25 +0900

    naver 소셜 로그인 설정

--- src/main/java/com/springboot/book/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/book/config/auth/dto/OAuthAttributes.java
@@ -26,6 +26,9 @@ public class OAuthAttributes {
 
     // of() OAuth2User에서 반환하는 정보는 Map이기 때문에 하나하나를 변환해야 한다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -38,7 +41,16 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
-
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object>attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
     //User 엔티티 설정.
     //OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입 할 때.
     //가입시 기본 권한을 GUEST로 주기 위해 role빌더 값에 Role.GUEST를 사용함.
